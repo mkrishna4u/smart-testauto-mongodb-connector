86ecc0114ca68d711fb9a15f07b1c4610afefd82
Madhav Krishna <madhav.krishna@gmail.com>
2023-07-24 15:13:23 -0400

    Updated the license disclaimer on all source files.

--- src/main/java/org/uitnet/testing/smartfwk/database/MongoDatabaseActionHandler.java
+++ src/main/java/org/uitnet/testing/smartfwk/database/MongoDatabaseActionHandler.java
@@ -1,6 +1,6 @@
 /*
  * SmartTestAutoFramework
- * Copyright 2023 and beyond
+ * Copyright 2023 and beyond [Madhav Krishna]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
